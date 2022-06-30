49387c9868380660e7b7b7849e38dc24c97140cf
Artur <94054303+awasilewsk1@users.noreply.github.com>
2022-06-30 14:13:40 +0200

    Add files via upload

---
+++ NET-INIS4_PR2.2-z4-main/NET-INIS4_PR2.2-z4-main/Model.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_INIS4_PR2._2_z4
+{
+    class Model
+    {
+        /*Prawdopodobnie brakuje jawnego pola z listą, właściwości jawnie zaimplementowanej i notyfikacji o zmianie*/
+        public LinkedList<Osoba> Lista { get; set; } = new LinkedList<Osoba>(new Osoba[] {
+            new Osoba() {
+                Tytuł = "Spider-man",
+                Reżyser = "Stan Lee",
+                Wydawca = "Story House Egmont",
+                Nośnik = "DVD",
+                DataNagrania = DateTime.Parse("29.09.2006")
+            },
+            new Osoba() {
+                Tytuł = "Pulp Fiction",
+                Reżyser = "Quentin Tarantino",
+                Wydawca = "A Band Apart Jersey Films",
+                Nośnik = "DVD",
+                DataNagrania = DateTime.Parse("14.09.1994")
+            },
+            new Osoba() {
+                Tytuł = "Avatar",
+                Reżyser = " James Cameron",
+                Wydawca = "20th Century Fox",
+                Nośnik = "BluRay i DVD",
+                DataNagrania = DateTime.Parse("29.12.2009")
+            },
+        });
+
+        internal void OtwórzSzczegóły(Osoba wybrany)
+        {
+            Szczegóły szczegóły = new Szczegóły(wybrany);
+            szczegóły.Show();
+        }
+        internal void DodajNowy()
+        {
+            Osoba nowa = new Osoba();
+            Lista.AddLast(nowa);
+            Szczegóły szczegóły = new Szczegóły(nowa);
+            szczegóły.Show();
+            /*aktualizowanie widoku samej listy*/
+        }
+    }
+}
